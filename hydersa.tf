d7aeba39a627d658476da31b613e4998914dfeda
root <root@ip-172-31-27-122.eu-west-2.compute.internal>
2021-04-25 18:41:18 +0000

    this is first commit

---
+++ hydersa.tf
@@ -0,0 +1,14 @@
+provider "aws" {
+region = "eu-west-2"
+access_key = ""
+secret_key = ""
+}
+resource "aws_elasticache_cluster" "sam_cluster" {
+cluster_id = "sameer"
+engine = "redis"
+node_type = "cache.t2.micro"
+num_cache_nodes = "1"
+parameter_group_name = "default.redis3.2"
+engine_version = "3.2.10"
+port = "6379"
+}
