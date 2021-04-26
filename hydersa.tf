2d55a09f3e1143a9cd5f16ae3afea8fbf95da07f
sameerqa <69347588+sameerqa@users.noreply.github.com>
2021-04-26 15:24:01 -0400

    Update hydersa.tf

--- hydersa.tf
+++ hydersa.tf
@@ -1,7 +1,7 @@
 provider "aws" {
 region = "eu-west-2"
 access_key = "AKIAX4EFVBORR4JO6UHJ"
-secret_key = "mRyXNCdLySY3q8hjpLWEh66YsssB9VVm5Yn7KWl"
+secret_key = "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"
 }
 resource "aws_s3_bucket" "mybucket" {
 bucket = "imran6667788ii1982"
